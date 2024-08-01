d8d8681feea89dbcca2f86832bfdc8a6518d4505
guidozoppo <1310.guido@gmail.com>
2024-08-01 17:20:51 -0300

    Types added

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,5 +1,39 @@
 interface Product {
-  productName: string, 
-  quantity: number, 
+  name: string, 
   unitPrice: number
+}
+
+interface SaleProduct extends Product {
+  quantity: number
+}
+
+interface ProductWithID extends Product {
+  id: number
+}
+
+interface InventoryProduct extends ProductWithID {
+  category: string,
+  stock: number,
+  expire: Date | null
+}
+
+interface Customer {
+  name: string,
+  email: string,
+  phone: string,
+}
+
+interface CustomerWithID extends Customer {
+  id: number
+}
+
+interface Category {
+  name: string
+}
+
+interface Sale {
+  date: string,
+  total: number,
+  customer_id: number,
+  products: number[]
 }
