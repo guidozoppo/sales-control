ec0514aceb550da8c06a92239ebea44d6a76d610
guidozoppo <1310.guido@gmail.com>
2024-08-01 17:21:04 -0300

    App and Main modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import { Header } from "./Components/Header"
+import { Header } from "./Components/commonComponents/Header"
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { MainMenu } from "./Components/MainMenu";
 import './App.css'
-import { Footer } from "./Components/Footer";
+import { Footer } from "./Components/commonComponents/Footer";
 import { Routes, Route } from 'react-router-dom';
 import { RegisterSale } from "./Components/Sections/RegisterSale";
-import { LoadProduct } from "./Components/Sections/LoadProduct";
-import { Customers } from "./Components/Sections/Customers"
+import { CreateProduct } from "./Components/Products/CreateProduct";
+import { Customers } from "./Components/Sections/Customer/Customers"
 import { SalesReports } from "./Components/Sections/SalesReports";
+import { AddCustomer } from "./Components/Sections/Customer/AddCustomer";
+import { Inventory } from "./Components/Sections/inventory/Inventory";
+import { CreateCategory } from "./Components/Category/createCategory";
 
 function App() {
   return (
@@ -16,8 +19,11 @@ function App() {
         <Routes>
           <Route path="/" element={<MainMenu />}/>
           <Route path="/registerSale" element={<RegisterSale />}/>
-          <Route path="/loadProducts" element={<LoadProduct />}/>
+          <Route path="/inventory" element={<Inventory />}/>
+          <Route path="/createProduct" element={<CreateProduct />}/>
+          <Route path="/createCategory" element={<CreateCategory />}/>
           <Route path="/customers" element={<Customers />}/>
+          <Route path="/addCustomer" element={<AddCustomer />}/>
           <Route path="/salesReports" element={<SalesReports />}/>
         </Routes>
       <Footer />
