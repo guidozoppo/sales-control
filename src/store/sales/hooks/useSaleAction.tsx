47d156c2111e9b97f025ff212ab1b0ff5e0f6be5
guidozoppo <1310.guido@gmail.com>
2024-09-04 15:05:24 -0300

    Add new features to register sale

--- src/store/sales/hooks/useSaleAction.tsx
+++ src/store/sales/hooks/useSaleAction.tsx
@@ -5,7 +5,6 @@ export const useSaleActions = () => {
   const dispatch = useDispatch()
 
   const registerSale = ({date, total, customer_id, products}: Sale) => {
-    console.log(date)
     dispatch(registerNewSale({date, total, customer_id, products}))
   }
 
